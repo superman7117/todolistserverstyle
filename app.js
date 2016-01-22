ba79c56ef21136bed13a3b45fed15a8e015661e2
Clark Ozanne <clarkozanne@gmail.com>
2016-01-22 11:56:34 -0800

    MVP

--- app.js
+++ app.js
@@ -32,17 +32,51 @@ app.get('/names', function(req, res) {
 app.post('/names', function(req, res) {
   fs.readFile('./names.json', function(err, data) {
     if(err) return res.status(400).send(err);
-    var arr = JSON.parse("\n\n"+data+"\n\n\n");
+    var arr = JSON.parse(data);
     console.log(req.body);
     var name = req.body.name;
-    var newDueDate =req.body.newDueDate
-    arr.push({'name': name, 'newDueDate': newDueDate});
+    var newDueDate = req.body.newDueDate
+    arr.push({name: name, newDueDate: newDueDate, complete: false});
     fs.writeFile('./names.json', JSON.stringify(arr), function(err) {
       if(err) return res.status(400).send(err);
       res.send(name = 'recieved.\n');
     });
   });
 });
+
+app.post('/names/delete', function(req, res, next) {
+	fs.readFile('./names.json', function(err, data) {
+		if (err) return res.status(400).send(err);
+		var arr = JSON.parse(data);
+		var index = parseInt(req.body.index);
+		var removed = arr.splice(index, 1);
+		fs.writeFile('./names.json', JSON.stringify(arr), function(err, data) {
+			if (err) return res.status(400).send(err);
+			res.send(removed);
+		});
+	})
+})
+
+app.post('/names/complete', function(req, res, next) {
+	fs.readFile('./names.json', function(err, data) {
+		if (err) return res.status(400).send(err);
+		var arr = JSON.parse(data);
+    console.log(arr[0]);
+		var index = parseInt(req.body.index);
+    console.log(arr[index].name);
+
+    console.log("THE THING!:", arr[index])
+    if (arr[index].complete === 'nono') {
+			arr[index].complete = 'yesyes';
+		} else {
+			arr[index].complete = 'nono';
+		}
+		fs.writeFile('./names.json', JSON.stringify(arr), function(err, data) {
+			if (err) return res.status(400).send(err);
+			res.send(arr[index].complete);
+		});
+	})
+})
 
 
 // spin up server
--- public/js/main.js
+++ public/js/main.js
@@ -6,11 +6,40 @@ function init() {
   populateNames();
   $('#newItemBTN').on('click', addName)
   $('ul').on('click', ".delete", removeItem)
+  $('ul').on('click', ".complete", completeItem)
 }
 
 function removeItem(){
-    var index = $(this).closest('.item').index()
-    console.log(index);
+    var index = $(this).closest('.fuckingThisOne').index();
+    $.post('/names/delete', {index: index})
+    .success( function(data){
+      $('ul').empty();
+      populateNames();
+    })
+    .fail(function(err) {
+     alert('something went wrong :(')
+   });
+
+}
+function completeItem(){
+    var index = $(this).closest('.fuckingThisOne').index();
+    var grabIt =$(this).closest('.fuckingThisOne')
+    $.post('/names/complete', {index: index})
+    .success( function(data){
+      console.log('what is this', data);
+      // if(data === true){
+      //   grabIt.closest('.fuckingThisOne').addClass('completed')
+      // }
+      // else{
+      //   grabIt.closest('.fuckingThisOne').removeClass('completed')
+      // }
+      $('ul').empty();
+      populateNames();
+    })
+    .fail(function(err) {
+     alert('something went wrong :(')
+   });
+
 }
 
 function addName(){
@@ -19,7 +48,7 @@ function addName(){
   var stringy = JSON.stringify({name: newName, dueDate: newDueDate})
   $.post('/names', {name: stringy})
   .success( function(data){
-    var $li = $('<li>').append($('<div>').addClass('item').append(newName))
+    var $li = $('<li>').addClass('fuckingThisOne').append($('<div>').addClass('item').append(newName))
     .append($('<div>').addClass('date').append(newDueDate))
     .append($('<div>').addClass('done').append($('<button>').addClass('complete')
     .text('Complete')).append($('<button>').addClass('delete').text('Delete')));
@@ -35,9 +64,10 @@ $.get('/names', function(data){
       console.log(data);
   var $names = data.forEach(function(name){
     var objr = JSON.parse(name.name);
+    var objc = name.complete
     console.log(objr.name, 'name')
     console.log(objr.dueDate, 'date');;
-    var $li = $('<li>').append($('<div>').addClass('item').append(objr.name))
+    var $li = $('<li>').addClass('fuckingThisOne '+name.complete).append($('<div>').addClass('item').append(objr.name))
     .append($('<div>').addClass('date').append(objr.dueDate))
     .append($('<div>').addClass('done').append($('<button>').addClass('complete')
     .text('Complete')).append($('<button>').addClass('delete').text('Delete')));
