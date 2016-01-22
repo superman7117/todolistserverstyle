d08accacd36f013fb6f5494f3cb446276b49a382
Clark Ozanne <clarkozanne@gmail.com>
2016-01-22 09:41:24 -0800

    try for index

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,18 @@ $(document).ready(init);
 function init() {
   populateNames();
   $('#newItemBTN').on('click', addName)
+  $('ul').on('click', ".delete", removeItem)
+}
+
+function removeItem(){
+    var index = $(this).closest('.item').index()
+    console.log(index);
 }
 
 function addName(){
   var newName = $('#newItem').val();
   var newDueDate = $('#newDue').val();
-  var stringy = JSON.stringify("name: "+newName+", dueDate: "+newDueDate)
+  var stringy = JSON.stringify({name: newName, dueDate: newDueDate})
   $.post('/names', {name: stringy})
   .success( function(data){
     var $li = $('<li>').append($('<div>').addClass('item').append(newName))
@@ -26,16 +32,17 @@ $('#output').append($li);
 
 function populateNames(){
 $.get('/names', function(data){
-      console.log(data[0].name);
+      console.log(data);
   var $names = data.forEach(function(name){
     var objr = JSON.parse(name.name);
-    console.log(objr);
-    var $li = $('<li>').append($('<div>').addClass('item').append(newName))
-    .append($('<div>').addClass('date').append(newDueDate))
+    console.log(objr.name, 'name')
+    console.log(objr.dueDate, 'date');;
+    var $li = $('<li>').append($('<div>').addClass('item').append(objr.name))
+    .append($('<div>').addClass('date').append(objr.dueDate))
     .append($('<div>').addClass('done').append($('<button>').addClass('complete')
     .text('Complete')).append($('<button>').addClass('delete').text('Delete')));
-$('#output').append($li);
+
+$('#output').append($li)
   })
-  $('#output').append($names)
 })
 }
